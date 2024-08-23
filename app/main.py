b999926fdccac0c0474a1eeb6da61588fd669c2a
SolomonZinabu <solomonzinabu56@gmail.com>
2024-08-23 09:11:08 +0300

    ..

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def load_data():
-    # Load your data here
     return pd.read_csv('../data/benin-malanville.csv')
 
 
@@ -12,11 +11,9 @@ def main():
     st.title("Solar Farm Data Dashboard")
     data = load_data()
     
-    # Example interactive element
     st.sidebar.header('User Input')
     selected_metric = st.sidebar.selectbox('Select Metric', ['GHI', 'DNI', 'DHI'])
 
-    # Plotting
     if selected_metric:
         st.write(f"### {selected_metric} Over Time")
         st.line_chart(data[selected_metric])
